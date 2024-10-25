1efb5c4c19c6a8b1d5687cb734984fbe08ebf266
MananJiwnani <mananjiwnani@gmail.com>
2024-10-25 19:03:11 +0530

    web design

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+import uuid
+import model
+
+app = Flask(__name__)
+
+svc, tfidf_vectorizer = model.train_data()
+
+@app.route('/', methods=["GET","POST"])
+def home():
+    if (request.method=="GET"):
+        return render_template("home.html")
+    elif (request.method=="POST"):
+        email_content = request.form.get('email_text')
+        result = model.training(email_content , svc, tfidf_vectorizer)
+        return render_template("result.html", email_type = result, email=email_content)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
